9b42c90a1c9f541a6fa30d178643204e20a0c571
Igor Butuzov <ibutuzov87@gmail.com>
2023-01-04 19:43:42 +0200

    update start files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+
+console.log(SimpleLightbox);
+console.log(Notify);
+console.log(Confirm);
+
+const axios = require('axios');
+
+console.log(axios);
